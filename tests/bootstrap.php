d01cc5aecd716f42ef90567a77141c8338f68d3f
Carl Alexander <carlalexander@gmail.com>
2015-09-25 10:04:37 -0400

    Try to fix vendor autoload

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 # bootstrap.php
 
-require_once(dirname(__DIR__) . '/../vendor/autoload.php');
+require_once(dirname(__DIR__) . '/vendor/autoload.php');
 
 $_tests_dir = getenv('WP_TESTS_DIR');
 if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
